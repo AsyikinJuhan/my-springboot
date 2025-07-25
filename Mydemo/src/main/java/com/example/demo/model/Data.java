14c8e52da0c8a2735cce09f6d3eb74ea81f19980
Your Name <you@example.com>
2025-07-25 15:59:06 +0800

    update

---
+++ Mydemo/src/main/java/com/example/demo/model/Data.java
@@ -0,0 +1,25 @@
+package com.example.demo.model;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicLong;
+
+public class Data {
+
+	private Data() {
+		// Prevent instantiation
+	}
+
+	private static final Map<Long, String> dataStore = new ConcurrentHashMap<>();
+
+	public static Map<Long, String> getDataStore() {
+	    return dataStore;
+	}
+	
+	private static final AtomicLong idCounter = new AtomicLong();
+	
+	public static AtomicLong getIdCounter() {
+	    return idCounter;
+	}
+
+}
